64be86f3b2b07df2814674e447c0617d1a78fa3e
Lukasz Walicki <lukasz.walicki@nursa.com>
2022-06-22 18:10:21 +0200

    game state created

---
+++ libs/rock-paper-scissors/src/lib/application/state/game.state-module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { GameState } from './game.state';
+
+@NgModule({ imports: [],
+  	declarations: [],
+  	providers: [GameState],
+  	exports: [] })
+export class GameStateModule {
+}
