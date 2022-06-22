745a0b49c9ee0a4af9f2251bb6f9fc31256ec722
Lukasz Walicki <lukasz.walicki@nursa.com>
2022-06-22 18:12:23 +0200

    loginUserResolver export

--- libs/users/src/index.ts
+++ libs/users/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/adapters/primary/components/login-user.component-module';
 export * from './lib/adapters/secondary/services/firebase-users.service-module';
 export * from './lib/adapters/primary/components/login-user.component-module';
 export * from './lib/application/state/user.state-module';
+export * from './lib/adapters/primary/resolvers/login-user.resolver';
+export * from './lib/adapters/primary/resolvers/login-user.resolver-module';
