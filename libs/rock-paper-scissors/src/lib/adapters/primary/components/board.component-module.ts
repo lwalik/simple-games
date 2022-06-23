6130fa548cc90697054fd4eebe46d7a0e7dfa0a5
Lukasz Walicki <lukasz.walicki@nursa.com>
2022-06-23 21:52:22 +0200

    first look boardComponent layout

--- libs/rock-paper-scissors/src/lib/adapters/primary/components/board.component-module.ts
+++ libs/rock-paper-scissors/src/lib/adapters/primary/components/board.component-module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardComponent } from './board.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
-@NgModule({ imports: [CommonModule],
-  	declarations: [BoardComponent],
-  	providers: [],
-  	exports: [BoardComponent] })
-export class BoardComponentModule {
-}
+@NgModule({
+  imports: [CommonModule, MatCardModule, MatButtonModule],
+  declarations: [BoardComponent],
+  providers: [],
+  exports: [BoardComponent],
+})
+export class BoardComponentModule {}
