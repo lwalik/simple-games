28291b194130c58dcecb43743ca5326ceb3a9fc8
Lukasz Walicki <lukasz.walicki@nursa.com>
2022-06-24 01:07:31 +0200

    joinGameComponent layout changed

--- libs/rock-paper-scissors/src/lib/adapters/primary/components/join-game.component-module.ts
+++ libs/rock-paper-scissors/src/lib/adapters/primary/components/join-game.component-module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JoinGameComponent } from './join-game.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   declarations: [JoinGameComponent],
   providers: [],
   exports: [JoinGameComponent],
