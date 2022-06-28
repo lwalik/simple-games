d7e4811c81bf30fb90a51c920e56ff3cf32f9991
Lukasz Walicki <lukasz.walicki@nursa.com>
2022-06-28 19:20:36 +0200

    joinGameComponent logic changed

--- libs/rock-paper-scissors/src/lib/adapters/primary/components/join-game.component.ts
+++ libs/rock-paper-scissors/src/lib/adapters/primary/components/join-game.component.ts
@@ -6,6 +6,7 @@ import {
 } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PlayerInContextQuery } from '../../../application/ports/primary/query/player-in-context.query';
+import { InGameQuery } from '../../../application/ports/primary/query/in-game.query';
 import {
   JOIN_PLAYER_COMMAND,
   JoinPlayerCommandPort,
@@ -14,6 +15,16 @@ import {
   GETS_CURRENT_PLAYER_IN_CONTEXT_QUERY,
   GetsCurrentPlayerInContextQueryPort,
 } from '../../../application/ports/primary/query/gets-current-player-in-context.query-port';
+import {
+  SET_OTHERS_PLAYER_IN_GAME_COMMAND,
+  SetOthersPlayerInGameCommandPort,
+} from '../../../application/ports/primary/command/set-others-player-in-game.command-port';
+import {
+  GETS_CURRENT_IN_GAME_QUERY,
+  GetsCurrentInGameQueryPort,
+} from '../../../application/ports/primary/query/gets-current-in-game.query-port';
+import { PlayerDTO } from '../../../application/ports/secondary/dto/player.dto';
+import { SetOthersPlayerInGameCommand } from '../../../application/ports/primary/command/set-others-player-in-game.command';
 import { JoinPlayerCommand } from '../../../application/ports/primary/command/join-player.command';
 
 @Component({
@@ -25,15 +36,25 @@ import { JoinPlayerCommand } from '../../../application/ports/primary/command/jo
 export class JoinGameComponent {
   player$: Observable<PlayerInContextQuery> =
     this._getsCurrentPlayerInContextQuery.getCurrentPlayerInContextQuery();
+  inGame$: Observable<InGameQuery> =
+    this._getsCurrentInGameQuery.getCurrentInGameQuery();
 
   constructor(
     @Inject(JOIN_PLAYER_COMMAND)
     private _joinPlayerCommand: JoinPlayerCommandPort,
     @Inject(GETS_CURRENT_PLAYER_IN_CONTEXT_QUERY)
-    private _getsCurrentPlayerInContextQuery: GetsCurrentPlayerInContextQueryPort
+    private _getsCurrentPlayerInContextQuery: GetsCurrentPlayerInContextQueryPort,
+    @Inject(SET_OTHERS_PLAYER_IN_GAME_COMMAND)
+    private _setOthersPlayerInGameCommand: SetOthersPlayerInGameCommandPort,
+    @Inject(GETS_CURRENT_IN_GAME_QUERY)
+    private _getsCurrentInGameQuery: GetsCurrentInGameQueryPort
   ) {}
 
-  onJoinButtonClicked(): void {
-    this._joinPlayerCommand.joinPlayer(new JoinPlayerCommand()).subscribe();
+  onJoinButtonClicked(player: PlayerDTO): void {
+    this._setOthersPlayerInGameCommand
+      .setOthersPlayerInGame(new SetOthersPlayerInGameCommand(player))
+      .subscribe(() =>
+        this._joinPlayerCommand.joinPlayer(new JoinPlayerCommand()).subscribe()
+      );
   }
 }
