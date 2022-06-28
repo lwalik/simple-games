c9dcff6f7e3625f926414d5ca90844ce4a52bf13
Lukasz Walicki <lukasz.walicki@nursa.com>
2022-06-28 19:19:25 +0200

    selectPlayerCountComponent - visible only for logged user

--- libs/rock-paper-scissors/src/lib/adapters/primary/components/select-player-count.component.ts
+++ libs/rock-paper-scissors/src/lib/adapters/primary/components/select-player-count.component.ts
@@ -10,6 +10,11 @@ import {
   SelectPlayersCountCommandPort,
 } from '../../../application/ports/primary/command/select-players-count.command-port';
 import { SelectPlayersCountCommand } from '../../../application/ports/primary/command/select-players-count.command';
+import {
+  SelectsUserContextPort,
+  SELECTS_USER_CONTEXT,
+} from 'libs/core/src/lib/application/ports/secondary/context/selects-user.context-port';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'lib-select-player-count',
@@ -20,9 +25,15 @@ import { SelectPlayersCountCommand } from '../../../application/ports/primary/co
 export class SelectPlayerCountComponent {
   readonly form: FormGroup = new FormGroup({ maxPlayers: new FormControl() });
 
+  isUser$: Observable<boolean> = this._selectsUserContext
+    .select()
+    .pipe(map((user) => (user.email.length !== 0 ? true : false)));
+
   constructor(
     @Inject(SELECT_PLAYERS_COUNT_COMMAND)
-    private _selectPlayersCountCommand: SelectPlayersCountCommandPort
+    private _selectPlayersCountCommand: SelectPlayersCountCommandPort,
+    @Inject(SELECTS_USER_CONTEXT)
+    private _selectsUserContext: SelectsUserContextPort
   ) {}
 
   onMaxPlayersSelected(form: FormGroup): void {
