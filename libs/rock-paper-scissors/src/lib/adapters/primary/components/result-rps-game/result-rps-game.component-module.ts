a65497189e52f582885e166af0e2f734e0917230
Lukasz Walicki <lukasz.walicki@nursa.com>
2022-07-01 20:52:24 +0200

    resultRpsGameComponent created

---
+++ libs/rock-paper-scissors/src/lib/adapters/primary/components/result-rps-game/result-rps-game.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ResultRpsGameComponent } from './result-rps-game.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ResultRpsGameComponent],
+  providers: [],
+  exports: [ResultRpsGameComponent],
+})
+export class ResultRpsGameComponentModule {}
