b78ab2b3ad0a024d9a6db217009cfe5cf0f96089
Lukasz Walicki <lukasz.walicki@nursa.com>
2022-07-22 08:29:03 +0200

    firebaseHangmanGameService created

--- libs/hangman/src/index.ts
+++ libs/hangman/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/adapters/primary/components/letters/letters.component-modul
 export * from './lib/application/state/hangman-game.state-module';
 export * from './lib/adapters/primary/components/secret-word/secret-word.component-module';
 export * from './lib/adapters/secondary/storages/hangman-game/in-memory-hangman-game.storage-module';
+export * from './lib/adapters/secondary/services/hangman-game/firebase-hangman-game.service-module';
