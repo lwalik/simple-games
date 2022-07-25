242f17b3456ed17d1d9aef1cb2768dfe838034f3
Lukasz Walicki <lukasz.walicki@nursa.com>
2022-07-25 20:05:01 +0200

    inMemoryHangmanGame created

--- libs/hangman/src/lib/adapters/secondary/storages/hangman-game/in-memory-hangman-game.storage.ts
+++ libs/hangman/src/lib/adapters/secondary/storages/hangman-game/in-memory-hangman-game.storage.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable, ReplaySubject, Subject, of } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 import { SetsStateHangmanGameContextPort } from '../../../../application/ports/secondary/context/sets-state-hangman-game.context-port';
-import { PatchesHangmanGameContextPort } from '../../../../application/ports/secondary/context/patches-hangman-game.context-port';
 import { SelectsHangmanGameContextPort } from '../../../../application/ports/secondary/context/selects-hangman-game.context-port';
+import { PatchesHangmanGameContextPort } from '../../../../application/ports/secondary/context/patches-hangman-game.context-port';
 import { HangmanGameContext } from '../../../../application/ports/secondary/context/hangman-game.context';
 
 @Injectable()
 export class InMemoryHangmanGameStorage
   implements
     SetsStateHangmanGameContextPort,
-    PatchesHangmanGameContextPort,
-    SelectsHangmanGameContextPort
+    SelectsHangmanGameContextPort,
+    PatchesHangmanGameContextPort
 {
   private _subject: Subject<HangmanGameContext> =
     new ReplaySubject<HangmanGameContext>(1);
@@ -20,14 +20,14 @@ export class InMemoryHangmanGameStorage
     return of(this._subject.next(state)).pipe(map(() => void 0));
   }
 
+  select(): Observable<HangmanGameContext> {
+    return this._subject.asObservable();
+  }
+
   patch(state: Partial<HangmanGameContext>): Observable<void> {
     return this._subject.pipe(
       take(1),
       map((context) => this._subject.next({ ...context, ...state }))
     );
-  }
-
-  select(): Observable<HangmanGameContext> {
-    return this._subject.asObservable();
   }
 }
