3c58cd9d27a064151314f084b4e9aed872df9ebb
Lukasz Walicki <lukasz.walicki@nursa.com>
2022-08-02 23:08:30 +0200

    hangmanGameContext changed

--- libs/hangman/src/lib/application/ports/secondary/context/hangman-game/hangman-game.context.ts
+++ libs/hangman/src/lib/application/ports/secondary/context/hangman-game/hangman-game.context.ts
@@ -1,6 +1,8 @@
 export interface HangmanGameContext {
   readonly username: string;
-  readonly level: string;
+  readonly selectedLevel: string;
   readonly selectedLetters: string[];
   readonly words: string[];
+  readonly currentWord: string;
+  readonly livesCount: number;
 }
