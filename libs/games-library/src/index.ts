339360f652d3e7ebcafcd61ffbe5b236326d76b7
Lukasz Walicki <lukasz.walicki@nursa.com>
2022-08-19 01:08:09 +0200

    feat(hangman): export for selectDifficultyLevelComponent added

--- libs/games-library/src/index.ts
+++ libs/games-library/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/adapters/secondary/services/firebase-games.service-module';
 export * from './lib/adapters/primary/components/multi-games-list.component-module';
 export * from './lib/adapters/primary/components/single-games-list/single-games-list.component-module';
+export * from './lib/adapters/primary/components/select-difficulty-level/select-difficulty-level.component-module';
